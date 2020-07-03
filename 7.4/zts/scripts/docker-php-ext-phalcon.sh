4cdeda15346adf6ceed00f82ee915ad590a18018
akai-z <akai-z@users.noreply.github.com>
2020-07-03 22:43:01 +0300

    Add PHP 7.4 ZTS Phalcon install shell script

---
+++ 7.4/zts/scripts/docker-php-ext-phalcon.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eo pipefail
+
+readonly VERSION="tags/v4.0.0"
+readonly REPOSITORY="phalcon/cphalcon"
+readonly SOURCE="https://api.github.com/repos/${REPOSITORY}/releases/${VERSION}"
+readonly SOURCE_DIR="docker-php-ext-cphalcon"
+
+apk add -u --no-cache --virtual .build-deps \
+  autoconf \
+  bash \
+  file \
+  g++ \
+  gcc \
+  make \
+  pcre-dev \
+  re2c
+
+mkdir $SOURCE_DIR
+cd $SOURCE_DIR
+
+curl $SOURCE \
+  | grep "tarball_url" \
+  | cut -d '"' -f 4 \
+  | xargs curl -L \
+  | tar -zx --strip 1
+
+cd build
+./install
+
+cd ../..
+rm -rf $SOURCE_DIR
+
+docker-php-ext-enable phalcon
