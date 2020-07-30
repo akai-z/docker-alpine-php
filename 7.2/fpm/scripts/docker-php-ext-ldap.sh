5185cae9a53be472bca7194736f656a131cf8f2a
akai-z <akai-z@users.noreply.github.com>
2020-07-30 21:56:32 +0300

    Fix PHP 7.2 FPM LDAP install shell script

--- 7.2/fpm/scripts/docker-php-ext-ldap.sh
+++ 7.2/fpm/scripts/docker-php-ext-ldap.sh
@@ -2,7 +2,8 @@
 
 set -eo pipefail
 
-apk add -u --no-cache --virtual .build-deps \
-  openldap-dev
+apk add -u --no-cache --virtual .build-deps openldap-dev
+apk add -u --no-cache libldap
 
+NPROC=$(getconf _NPROCESSORS_ONLN)
 docker-php-ext-install -j${NPROC} ldap
